9bf1256a3465e10ae0571c9094405888760b9219
lsem <lyubomyr.semkiv@hotmail.com>
2019-09-13 01:13:35 +0300

    Integrate llvm-expected

---
+++ src/common/include/common/pch.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <llvm-expected/llvm-expected.h>
+
+#include <string>
+#include <vector>
+#include <map>
+#include <set>
+#include <unordered_set>
+#include <unordered_map>
+#include <cstdint>
+#include <algorithm>
+
+using llvm::Expected;
+using llvm::Error;
+
+using std::string;
+using std::vector;
+using std::map;
+using std::set;
+using std::unordered_set;
+using std::unordered_map;
